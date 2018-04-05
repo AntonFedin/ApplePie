e2fd4a9e4d2641084a817023c18cde3615e66b73
student <student@student-16.local>
2018-04-05 16:53:53 +0300

    добавлен код

--- ApplePaiAnton/ApplePaiAnton/ViewController.swift
+++ ApplePaiAnton/ApplePaiAnton/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWorldLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 
+    var listOfWords = ["Анастасия", "Анна" , "Мария"]
+    let incorrectMovesAllowed = 7
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var currentGame: Game!
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
     }
-
-
+    func updateUI() {
+        let name = "Tree \(currentGame.incorrectMovesRemaining)"
+        treeImageView.image = UIImage(named: name)
+    }
+ 
 }
-
---
+++ ApplePaiAnton/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  ApplePaiAnton
+//
+//  Created by student on 05.04.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
